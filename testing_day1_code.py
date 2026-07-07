8e21a9c9355dc30bb2e76cd738f75c6c85cd2d54
AnuhskaSTilekar <anushka.tilekar19@gmail.com>
2026-07-07 11:39:54 +0000

    Renamed the folder "downloaded_data_files" to "downloaded_dataset_files". also made this correction wihtin testing_day1_code.py and checked that this day1 code testing file still works. I does. Yayy.

--- testing_day1_code.py
+++ testing_day1_code.py
@@ -8,7 +8,7 @@
 from dark_energy_evidence_package.data_loader import load_supernova_data
 from dark_energy_evidence_package.without_dark_energy import fit_no_dark_energy_line
 
-z, mu, mu_err = load_supernova_data('downloaded_data_files/our_new_custom_DES_SN5yr_dataset.csv')
+z, mu, mu_err = load_supernova_data('downloaded_dataset_files/our_new_custom_DES_SN5yr_dataset.csv')
 print(f'From the given input .csv file, Loaded {len(z)} supernovae, with redshfit z range: {z.min():.3f} to {z.max():.3f}')
 
 slope, intercept = fit_no_dark_energy_line(z, mu)
