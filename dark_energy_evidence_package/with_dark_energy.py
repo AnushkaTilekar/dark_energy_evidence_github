1fc635021f2fedcdf126e6313df8ab9d46a8f5e7
AnuhskaSTilekar <anushka.tilekar19@gmail.com>
2026-07-11 06:52:49 -0300

    renamed plotting.py to graphs.py everywhere and made grammatical corrections in the with_dark_energy.py file

--- dark_energy_evidence_package/with_dark_energy.py
+++ dark_energy_evidence_package/with_dark_energy.py
@@ -1,6 +1,6 @@
 # This .py file inputs an array of redshift values (z), plus two optional numbers (H0, Om0),
 # describing "what recipe of universe" to assume.
-# Then, the program menitoned in this file builds a mathematical model of an expanding universe 
+# Then, the program mentioned in this file builds a mathematical model of an expanding universe 
 # using Python's "astropy" library (or "Package")'s ready-made cosmology tools, then asks that 
 # model- "At each of these redshifts, how far away would the supernova with the given redshift 
 # value be in the universe from the observer (or telescope) that recorded this redshift value
@@ -11,7 +11,7 @@
 # 'values without any physical units (i.e. pure numbers)' for convenience, and returns these 
 # distance modulus values without any physical units (i.e. pure numbers) as output of this file. 
 # This output would then be used in one of the other files within this package, 
-# called "plotting.py", to compare against our real data (i.e. the dataset obtained from the 
+# called "graphs.py", to compare against our real data (i.e. the dataset obtained from the 
 # user in the data_loader.py file) and the naive straight line (i.e. plotted line obtained on 
 # our data by the absence of dark energy) - which is calculated in the file "without_dark_energy.py" 
 # in this package.
@@ -55,30 +55,30 @@ from astropy.cosmology import FlatLambdaCDM
 # This is Python's way of making an argument optional for the user.
 def predict_distance_modulus_with_dark_energy_presence(z, H0=67.66, Om0=0.3096):
     """
-    Predict the distane modulus assuming a universe containing dark energy.
+    Predict the distance modulus assuming a universe containing dark energy.
 
     Uses astropy's built-in FlatLambdaCDM cosmology model to compute the distane
     modulus expected at each input redshift, for a flat universe containing matter 
     (ordinary + dark) and dark energy, with an expansion rate set by H0.
-    This model assumes dark energy beahaves as a cosmological constant 
+    This model assumes dark energy behaves as a cosmological constant 
     (with the equation of state parameter for dark energy being defined here as w=-1),
     which is consistent with the Lambda-CDM picture.
 
-    NOTE: Om0 here prepresents the "total" matter density, including both- ordinary
+    NOTE: Om0 here represents the "total" matter density, including both- ordinary
     (i.e. baryonic) matter, and dark matter, combined. these two are not separated here 
     in the Friedmann Equations because, they affect the expansion history of the universe
     universe identically by the mathematics of the above considered assumptions.
     The remaining density  of the universe, i.e. (1-Om0), is assumed to be dark energy here,
     since a flat universe requires all density components to sum to 1.
 
-    The default values here aare derived from the same cosmological parameters
+    The default values here are derived from the same cosmological parameters
     used in this author's other CAMB-based project:
     (i) H0 = 67.66,
-    (ii) ombh2 = 0.2242,
+    (ii) ombh2 = 0.02242,
     (iii) omch2 = 0.11933.
     Converting these values to fractional densities (i.e. dividing by h^2, where
     h = H0/100) would give us:
-    Omo = (ombh2 + omch2) / h^2 ~= 0.3096.
+    Om0 = (ombh2 + omch2) / h^2 ~= 0.3096.
 
     Args:
         z (array): Redshift values at which we want to compute predictions.
@@ -106,28 +106,32 @@ def predict_distance_modulus_with_dark_energy_presence(z, H0=67.66, Om0=0.3096):
 
 
     
-    # Python's "astropy" library, has a built-in method called, "distmod()", in it's cosmology module's 
-    # (i.e. "subpackage's") "FlatLambdaCDM" class.
-    # The "distmod(z)" funciton tells our cosmology model, currently called, "our_custom_cosmology_model",
-    # to calculate the distance modulus at each specified redshift value 'z' form the input redshift 
-    # array. Remember, the redshift 'z' array being used here is take form the user's input dataset in the data_loader.py file.
+    # Python's "astropy" library, has a built-in method called, "distmod()", in its cosmology 
+    # module's (i.e. "subpackage's") "FlatLambdaCDM" class.
+    # The "distmod(z)" function tells our cosmology model, currently called, 
+    # "our_custom_cosmology_model", to calculate the distance modulus at each specified 
+    # redshift value 'z' form the input redshift array. Remember, the redshift 'z' array being 
+    # used here is take from the user's input dataset in the data_loader.py file.
     # Now, astropy is very careful about tracking physical units like "this number is in 
     # magnitudes", etc.
     # Here, astropy stores the result of calculated distance magnitude in units of "--". 
     # Hence, we store it in the variable called "predicted_mu_with_units".
     # Numbers stored with units, this is useful for us further in the package while creating and 
-    # displaying plots, in the file "plotting.py" to be precise, and also for keeping a check on 
+    # displaying plots, in the file "graphs.py" to be precise, and also for keeping a check on 
     # dimensional correctness in our mathematical calculations.
     # Here,
     # mu => "distance modulus"
     predicted_mu_with_units = our_custom_cosmology_model.distmod(z)
 
 
-    # But also, further in the package, in our calculations, we would need to use these obtained distance modulus numbers
-    # without their units - i.e. in pure number form - for some mathematical procedures.
-    # Hence, we now we use "valu()" funciton, which is a built-in function of the Pytohn's astropy lbrary.
-    # This .value at the end strips away the extra unit-tracking information which astropy had attached internally above 
-    # leaving us with plain numbers for the obtained distance modulus values for each given redshift value.
+    # But also, further in the package, in our calculations, we would need to use these obtained 
+    # distance modulus numbers without their units - i.e. in pure number form - for some 
+    # mathematical procedures.
+    # Hence, we now we use "value()" function, which is a built-in function of the Python's 
+    # astropy library.
+    # This .value at the end strips away the extra unit-tracking information which astropy had 
+    # attached internally above leaving us with plain numbers for the obtained distance modulus 
+    # values for each given redshift value.
     # Here,
     # mu => "distance modulus"
     predicted_mu_without_units = predicted_mu_with_units.value
@@ -135,8 +139,9 @@ def predict_distance_modulus_with_dark_energy_presence(z, H0=67.66, Om0=0.3096):
 
     
 
-    # This line sends the above obtained array of predicted distance modulus values (as pure numbers) back out to
-    # whichever variable or funciton called this "predict_distance_modulus_with_dark_energy()" function.
+    # This line sends the above obtained array of predicted distance modulus values (as pure numbers)
+    # back out to whichever variable or funciton called this 
+    # "predict_distance_modulus_with_dark_energy()" function.
     # Here,
     # mu => "distance modulus"
     return predicted_mu_without_units
@@ -164,6 +169,6 @@ def predict_distance_modulus_with_dark_energy_presence(z, H0=67.66, Om0=0.3096):
 # The remaining (1 - Om0) = dark energy 
 # (since the model assumes a flat universe, these two must add to 1).
 # This means here, dark matter isn't neglected, it's baked into Om0 by definition. 
-# Thus, in this ocde, "Om0 includes both ordinary matter and dark matter, along with dark energy".
+# Thus, in this code, "Om0 includes both ordinary matter and dark matter, along with dark energy".
 # And, FlatLambdaCDM in astropy already assumes w = -1 internally 
 # - it's baked into the model choice, not a separate parameter that user needs to set.
