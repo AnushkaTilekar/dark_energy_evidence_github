57d30967a58a1843c62c20504e2ffc548f8ba001
AnuhskaSTilekar <anushka.tilekar19@gmail.com>
2026-07-15 19:49:55 -0300

    tested all code created under day 2 agenda in the testing_day2_code.py file

---
+++ testing_day2_code.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from dark_energy_evidence_package.data_loader import load_supernova_data
+from dark_energy_evidence_package.without_dark_energy import fit_no_dark_energy_line
+from dark_energy_evidence_package.with_dark_energy import predict_distance_modulus_with_dark_energy_presence
+from dark_energy_evidence_package.graphs import plot_hubble_diagram, plot_residuals
+
+z, mu, mu_err = load_supernova_data('downloaded_dataset_files/our_new_custom_DES_SN5yr_dataset.csv')
+
+slope, intercept = fit_no_dark_energy_line(z, mu)
+
+z_grid = np.linspace(z.min(), z.max(), 200)
+
+dark_energy_mu = predict_distance_modulus_with_dark_energy_presence(z_grid)
+
+plot_hubble_diagram(z, mu, slope, intercept, z_grid, dark_energy_mu, save_path="testing_day2_hubble_diagram.png")
+plot_residuals(z, mu, slope, intercept, save_path="testing_day2_residuals.png")
+
+print("Done. Succesfully produced and saved testing_day2_hubble_diagram.png and testing_day2_residuals.png")
+
